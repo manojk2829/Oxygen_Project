00a0f4d52544d652642aa573d0021ce457774f60
Manoj Kushwaha <manojk2829@gmail.com>
2022-04-08 20:32:45 +0530

    GIT Commit

---
+++ Oxygen_Project/src/testManojKushwaha/GitUpdate.java
@@ -0,0 +1,15 @@
+package testManojKushwaha;
+
+public class GitUpdate {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		if(a>b) {
+			System.out.println("A is Big");
+		}else {
+			System.out.println("A is Small");
+		}
+	}
+
+}
