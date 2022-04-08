af87d53d542d948be28b864f77082ef648f2a8f7
Manoj Kushwaha <manojk2829@gmail.com>
2022-04-08 20:15:30 +0530

    Manoj New Commit

---
+++ Oxygen_Project/src/testManojKushwaha/NewClass.java
@@ -0,0 +1,16 @@
+package testManojKushwaha;
+
+public class NewClass {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		if(a>b) {
+			System.out.println("A is Big");
+		}else {
+			System.out.println("A is Small");
+		}
+
+	}
+
+}
